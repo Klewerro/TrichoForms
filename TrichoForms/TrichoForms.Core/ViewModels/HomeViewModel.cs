c081f313af79d86f22c3071d655101871117a557
PSzymon <pol.szym95@gmail.com>
2019-10-14 21:39:41 +0100

    HomeView with images carousel. Navigation to MainView.
    Images added to platform projects.
    Carousel home view created.
    Loading and binding images to CarouselView.
    
    MainViewViewModel have different name cause of naming conflict with android main activity.

--- TrichoForms/TrichoForms.Core/ViewModels/HomeViewModel.cs
+++ TrichoForms/TrichoForms.Core/ViewModels/HomeViewModel.cs
@@ -1,11 +1,42 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace TrichoForms.Core.ViewModels
 {
     public class HomeViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
+
+        public List<ImageSource> Images { get; set; }
+
+
+        public HomeViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            
+        }
+
+        public override void Prepare()
+        {
+            Images = new List<ImageSource>
+            {
+                ImageSource.FromFile("Carousel_image_1.png"),
+                ImageSource.FromFile("Carousel_image_2.png"),
+                ImageSource.FromFile("Carousel_image_3.png")
+            };
+        }
+
+
+        public IMvxCommand NavigateToMenuCommand => new MvxAsyncCommand(async () 
+            => await _navigationService.Navigate<MainViewViewModel>().ConfigureAwait(false));
+
+
     }
 }
---
+++ TrichoForms/TrichoForms.FormsUI/Views/MainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrichoForms.Core.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrichoForms.FormsUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : MvxContentPage<MainViewViewModel>
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
