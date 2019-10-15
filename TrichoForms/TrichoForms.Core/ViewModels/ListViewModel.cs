3eca56b23d12d1a3dbdc56b1ae52b7f315f218a8
PSzymon <pol.szym95@gmail.com>
2019-10-15 01:18:31 +0100

    ListView page created.
    Images are loading from json and are coded in Base64.
    Created converter for changing data from string (Base64) to ImageSource.

---
+++ TrichoForms/TrichoForms.Core/ViewModels/ListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using TrichoForms.Core.Interfaces;
+using TrichoForms.Core.Models;
+using TrichoForms.Core.Services;
+
+namespace TrichoForms.Core.ViewModels
+{
+    public class ListViewModel : ViewModelBase
+    {
+        private readonly IItemService _itemService;
+        private MvxObservableCollection<ListItem> _items;
+        private bool _isBusy;
+
+        public MvxObservableCollection<ListItem> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+
+        public ListViewModel(IMvxNavigationService navigationService, IItemService itemsService) : base(navigationService)
+        {
+            _itemService = itemsService;
+            Items = new MvxObservableCollection<ListItem>();
+        }
+
+        public override async Task Initialize()
+        {
+            await Task.Run(() =>
+            {
+                IsBusy = true;
+
+                Items.Clear();
+                var items = _itemService.GetItems();
+                Items.AddRange(items);
+
+                IsBusy = false;
+            });
+        }
+
+
+    }
+}
---
+++ TrichoForms/TrichoForms.FormsUI/Converters/ImageToImageSourceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TrichoForms.FormsUI.Converters
+{
+    public class ImageToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var imageString = value as string;
+            if (string.IsNullOrEmpty(imageString))
+                return null;
+
+            return ImageSource.FromStream(() => new MemoryStream(System.Convert.FromBase64String(imageString)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
