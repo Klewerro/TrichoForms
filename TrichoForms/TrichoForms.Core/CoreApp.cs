8355311b3fafd148b5d97cfaa6c7dcaac1957712
PSzymon <pol.szym95@gmail.com>
2019-10-15 10:36:00 +0100

    Displaying or not Homeview depends if it is first app start or not.
    If it is first application start, then HomeVIew will be displayed. Else it will not.

--- TrichoForms/TrichoForms.Core/CoreApp.cs
+++ TrichoForms/TrichoForms.Core/CoreApp.cs
@@ -1,11 +1,15 @@
 ﻿using MvvmCross.IoC;
 using MvvmCross.ViewModels;
 using TrichoForms.Core.ViewModels;
+using Xamarin.Essentials;
 
 namespace TrichoForms.Core
 {
     public class CoreApp : MvxApplication
     {
+        private const string FIRST_START_KEY = "first_start";
+
+
         public override void Initialize()
         {
             CreatableTypes()
@@ -13,7 +17,23 @@ namespace TrichoForms.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 
-            RegisterAppStart<HomeViewModel>();
+            AppStartRegistration();
+        }
+
+
+        private void AppStartRegistration()
+        {
+            var isFirstStart = Preferences.ContainsKey(FIRST_START_KEY);
+
+            if (isFirstStart)
+            {
+                RegisterAppStart<MainViewViewModel>();
+            }
+            else
+            {
+                RegisterAppStart<HomeViewModel>();
+                Preferences.Set(FIRST_START_KEY, true);
+            }
         }
 
     }
